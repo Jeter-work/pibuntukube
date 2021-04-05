9a610a0a944fcc2c0031f5f76503cab8b6b16505
Scott Jeter <devops@jeterworld.net>
2021-04-04 20:09:50 -0500

    convert commodore to pico

--- notused/copy-to-sd.sh
+++ notused/copy-to-sd.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # remount the sd card writable, and copy growroot-disable (disables cloud-init growroot and growpart)
-sudo mount -o remount,rw /media/commodore/writable
-sudo cp /home/commodore/pibuntu/local/custom/root/etc/growroot-disabled /media/commodore/writable/etc/
-sudo chmod 644 /media/commodore/writable/etc/growroot-disabled 
+sudo mount -o remount,rw /media/pico/writable
+sudo cp /home/pico/pibuntu/local/custom/root/etc/growroot-disabled /media/pico/writable/etc/
+sudo chmod 644 /media/pico/writable/etc/growroot-disabled 
 echo "should see growroot-disabled"
-ls -l /media/commodore/writable/etc/growroot-disabled
+ls -l /media/pico/writable/etc/growroot-disabled
 
 # copy customization cloud-init for network and users
-sudo cp /home/commodore/pibuntu/local/custom/root/etc/cloud/cloud.cfg.d/* /media/commodore/writable/etc/cloud/cloud.cfg.d/
+sudo cp /home/pico/pibuntu/local/custom/root/etc/cloud/cloud.cfg.d/* /media/pico/writable/etc/cloud/cloud.cfg.d/
 echo "should see cloud.cfg.d files"
-ls -l /media/commodore/writable/etc/cloud/cloud.cfg.d/
+ls -l /media/pico/writable/etc/cloud/cloud.cfg.d/
 
 # copy user-data file for hostname
-sudo cp /home/commodore/pibuntu/local/custom/system-boot/user-data /media/commodore/system-boot/
+sudo cp /home/pico/pibuntu/local/custom/system-boot/user-data /media/pico/system-boot/
 echo "should see user-data"
-ls -l /media/commodore/system-boot/user-dataD
+ls -l /media/pico/system-boot/user-dataD
